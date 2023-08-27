284a85582c27a2cdd02b0fd9f79b60c88157d272
Roberthall653 <roberthall653@gmail.com>
2023-08-27 09:18:54 -0700

    Put and Delete routes working

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -20,9 +20,11 @@ router
   .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').post(addReaction);
+router.route('/:thoughtId/reactions')
+.post(addReaction);
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
-router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(removeReaction);
 
 module.exports = router;
